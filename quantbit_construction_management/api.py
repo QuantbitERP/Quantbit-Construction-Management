49980b8fcc8884446604a872df39cb584eb0ab8a
vedikapatil27 <vedikapatil2713@gmail.com>
2026-05-15 14:43:35 +0530

    15/05/2026:TASK2604032:Create stage ,task and subtask in the project task details from existing stage,task and subtask

--- quantbit_construction_management/api.py
+++ quantbit_construction_management/api.py
@@ -47,3 +47,37 @@ def get_template_subtasks(doctype, txt, searchfield, start, page_len, filters):
         "start": start
 
     })
+
+
+@frappe.whitelist()
+def clone_task_hierarchy(source_task, target_project, parent_task=None, include_dependencies=False, include_children=False):
+    include_dependencies = frappe.parse_json(include_dependencies)
+    include_children = frappe.parse_json(include_children)
+
+    source_doc = frappe.get_doc("Task", source_task)
+
+    # Clone the Task
+    new_task = frappe.copy_doc(source_doc)
+    new_task.project = target_project
+    new_task.parent_task = parent_task
+    new_task.is_template = 0
+    new_task.status = "Open"
+    new_task.progress = 0
+    new_task.completed_by = None
+    new_task.completed_on = None
+    new_task.act_start_date = None
+    new_task.act_end_date = None
+
+    if not include_dependencies:
+        new_task.depends_on = []
+
+    new_task.insert()
+
+    if include_children:
+        # Get children
+        children = frappe.get_all("Task", filters={"parent_task": source_task}, fields=["name"])
+        for child in children:
+            # Recursive call: clone child and its children
+            clone_task_hierarchy(child.name, target_project, new_task.name, include_dependencies, True)
+
+    return new_task.name
